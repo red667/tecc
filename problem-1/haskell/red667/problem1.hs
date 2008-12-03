fdf019b6850b0832e66e662b639d46c976c5dfb2
Bernd Forstner <red667@hackerspaces.org>
2008-12-03 02:04:37 +0100

    foo

--- problem-1/haskell/red667/problem1.hs
+++
@@ -1,9 +0,0 @@
-import System.Environment
-
-main = print (problem1 3 0)
-
-problem1 :: Integer -> Integer -> Integer
-problem1 count sum
-	| count == 1000															= sum
-	| (mod count 3 == 0) || (mod count 5 == 0)	= problem1 (count + 1) (sum + count)
-	| otherwise																	= problem1 (count + 1) sum
--- problem-6/haskell/red667/problem6.hs
+++
@@ -1,3 +0,0 @@
-import System.Environment
-
-main = print ( (sum [1..100])^2 - sum (map (^2) [1..100]) )
