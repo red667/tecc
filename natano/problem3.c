00e21e871919f90dd7bd57d4f05f326e1e59b35e
natano <natanoptacek@gmail.com>
2009-01-20 05:26:03 +0100

    problem 3 works better

--- natano/problem3.c
+++ natano/problem3.c
@@ -5,39 +5,28 @@
 #define TRUE  1
 #define FALSE 0
 
-
-int is_prime(unsigned long long x)
-{
-    unsigned long long  i = x / 3;
-    if(i % 2 == 0) {
-        i--;
-    }
-    for(; i > 1; i -= 2) {
-        if(x % i == 0) {
-            return FALSE;
-        }
-    }
-    return TRUE;
-}
-
-
 int main(void)
 {
     unsigned long long x = 600851475143;
-    unsigned long long factor = 2;
+    unsigned long long y = x;
+    unsigned long long factor = 3;
+    unsigned long long i = 0;
     int solution = 0;
-    while(!(factor * factor > x)) {
+    while(!(factor * factor > y)) {
         if(x % factor == 0) {
-            x / factor;
-            if(is_prime(factor)) {
-                solution = factor;
+            i = factor / 3;
+            if(i % 2 == 0) {
+                i--;
             }
+            for(; i > 1; i -= 2) {
+                if(x % i == 0) {
+                    continue;
+                }
+            }
+            solution = factor;
+            x = x / factor;
         }
-        if(factor == 2) {
-            factor++;
-        } else {
-            factor += 2;
-        }
+        factor += 2;
     }
     printf("%d\n", solution);
 
