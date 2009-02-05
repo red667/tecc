19d8ff3d5b8a1a2806893221c736118b35096ad2
Michael Kohl <citizen428@gmail.com>
2009-02-06 00:45:03 +0100

    Problem 22 solved in Ruby

--- citizen428/1/euler1.rb
+++
@@ -1,8 +0,0 @@
-def calculate(limit)
-  sum = 0
-
-  (1...limit).each { |i| sum += i if i % 3 == 0 || i % 5 == 0 }
-  sum
-end
-
-puts calculate(1000)
--- citizen428/5/euler5.rb
+++
@@ -1,8 +0,0 @@
-require 'rational'
-
-limit = 20
-
-# finds lowest common multiple
-result = (1..limit).inject { |c,n| c.lcm n }
-
-puts "Smallest number divisible by all numbers from 1 to #{limit}: #{result}"
