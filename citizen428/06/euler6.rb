be0cc9d7a47a66133731655a45a93aacc9aa65d4
Michael Kohl <citizen428@gmail.com>
2009-05-20 00:00:03 +0200

    Updated Ruby version of problem 6

--- citizen428/06/euler6.rb
+++ citizen428/06/euler6.rb
@@ -1,16 +1,12 @@
 def sum_of_squares(limit)
-  sum = 0
-  (1..limit).each { |num| sum += num ** 2 }
-  sum
+  (1..limit).inject(0) { |sum, num| sum += num ** 2 }
 end
 
 def square_of_sum(limit)
-  sum = 0
-  (1..limit).each { |num| sum += num }
-  sum ** 2
+  ((1..limit).inject(0) { |sum, num| sum += num }) ** 2
 end
 
 limit = 100
 
-puts "Differences between the sum of squares and the square of sums of the first #{limit} natural number is: #{square_of_sum(limit) - sum_of_squares(limit)}"
+puts "Sum of squares - square of for 1 to #{limit}: #{square_of_sum(limit) - sum_of_squares(limit)}"
 
