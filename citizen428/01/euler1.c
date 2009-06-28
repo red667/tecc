75d264c5b3cbdcad903a99899fc926533651d049
Michael Kohl <citizen428@gmail.com>
2009-06-28 21:48:16 +0200

    Added C version, redid Ruby version

---
+++ citizen428/01/euler1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int sum = 0;
+  int i;
+
+  for (i=1; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0)
+      sum += i;
+  }
+  printf("%i\n", sum);
+  return 0;
+}
